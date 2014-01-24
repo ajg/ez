b3059f0d09dd9514da95c6bf0b8f0d9d863f920b
Alvaro J. Genial <genial@alva.ro>
2014-01-24 17:57:34 -0500

    Add test for Out

--- unit_test.go
+++ unit_test.go
@@ -12,3 +12,11 @@ func TestIn(t *testing.T) {
 		t.Errorf("In(%v)\nwant (%v)\nhave (%v)", args, cin, in)
 	}
 }
+
+func TestOut(t *testing.T) {
+	args := []interface{}{true, 42, "foo"}
+	cout := out{tuple{args}, "unit_test.go", 19, nil}
+	if out := Out(args...); !reflect.DeepEqual(cout, out) {
+		t.Errorf("Out(%v)\nwant (%v)\nhave (%v)", args, cout, out)
+	}
+}
