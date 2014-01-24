8eff784f13972c739ffd2da08939d1a8d69c7092
Alvaro J. Genial <genial@alva.ro>
2014-01-24 14:06:10 -0500

    Add header notices

--- ez.go
+++ ez.go
@@ -1,2 +1,6 @@
+// Copyright 2014 Alvaro J. Genial. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package ez provides an easy but powerful way to define unit tests and benchmarks that are compatible with package `testing`.
 package ez
