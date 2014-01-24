6817a7ae5f71776462e2308fe5e76ddde3e36641
Alvaro J. Genial <genial@alva.ro>
2014-01-24 16:21:49 -0500

    Document various Unit methods

--- unit.go
+++ unit.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 )
 
+// A Unit is a specification that can be used for testing and/or benchmarking.
 type Unit struct {
 	fn interface{}
 	rs []runner
@@ -18,16 +19,19 @@ type Unit struct {
 	br bool
 }
 
+// A half is an incomplete case.
 type half struct {
 	in in
 	u  *Unit
 }
 
+// A runner is one of many sequential components of a Unit.
 type runner interface {
 	runTest(int, *testing.T)
 	runBenchmark(int, *testing.B)
 }
 
+// New returns a blank Unit.
 func New() *Unit {
 	u := &Unit{}
 	// FIXME: Sadly, finalizers are not guaranteed to run, so they're of little comfort.
@@ -39,10 +43,12 @@ func New() *Unit {
 	return u
 }
 
+// Test returns a Unit for testing fn using t.
 func Test(fn interface{}, t *testing.T) *Unit {
 	return New().setT(t).Func(fn)
 }
 
+// Benchmark returns a Unit for benchmarking fn using b.
 func Benchmark(fn interface{}, b *testing.B) *Unit {
 	return New().setB(b).Func(fn)
 }
@@ -57,25 +63,30 @@ func (u *Unit) setB(b *testing.B) *Unit {
 	return u
 }
 
+// Func sets fn as the Unit's current function; it can be called more than once.
 func (u *Unit) Func(fn interface{}) *Unit {
 	u.fn = fn
 	return u
 }
 
+// Thru applies fn to the Unit, which is useful to combine or repeat common Unit components.
 func (u *Unit) Thru(fn func(*Unit)) *Unit {
 	fn(u)
 	return u
 }
 
+// Step adds fn to the Unit as a Step.
 func (u *Unit) Step(fn func()) *Unit {
 	u.rs = append(u.rs, Step{fn})
 	return u
 }
 
+// Case adds in & out (plus the current function) as a Case to the Unit.
 func (u *Unit) Case(in in, out out) *Unit {
 	return u.addCase(in, out)
 }
 
+// Cases adds every in/out pair in the CaseMap (plus the current function) as a Case to the Unit.
 func (u *Unit) Cases(cs CaseMap) *Unit {
 	for in, out := range cs {
 		u = u.addCase(*in, out)
@@ -87,15 +98,21 @@ func In(xs ...interface{}) in   { return newIn(xs) }
 func Out(xs ...interface{}) out { return newOut(xs) }
 func Panic(x interface{}) out   { return newPanic(x) }
 
-func (u *Unit) In(xs ...interface{}) *half  { return &half{newIn(xs), u} }
+// In begins a Case with xs as inputs.
+func (u *Unit) In(xs ...interface{}) *half { return &half{newIn(xs), u} }
+
+// Out completes a Case with xs as outputs and adds it to the Unit.
 func (h *half) Out(xs ...interface{}) *Unit { return h.u.addCase(h.in, newOut(xs)) }
-func (h *half) Panic(x interface{}) *Unit   { return h.u.addCase(h.in, newPanic(x)) }
+
+// Panic completes a Case that must panic with x and adds it to the Unit.
+func (h *half) Panic(x interface{}) *Unit { return h.u.addCase(h.in, newPanic(x)) }
 
 func (u *Unit) addCase(in in, out out) *Unit {
 	u.rs = append(u.rs, newCase(u.fn, in, out))
 	return u
 }
 
+// Run runs the Unit as a test and/or benchmark, depending on whether T and/or B are set.
 func (u *Unit) Run() {
 	if u.B == nil && u.T == nil {
 		panic("T and B are both nil")
@@ -108,6 +125,7 @@ func (u *Unit) Run() {
 	}
 }
 
+// RunTest runs the Unit as a test using t.
 func (u *Unit) RunTest(t *testing.T) {
 	if u.tr {
 		panic("test already ran")
@@ -119,6 +137,7 @@ func (u *Unit) RunTest(t *testing.T) {
 	}
 }
 
+// RunBenchmark runs the Unit as a benchmark using b.
 func (u *Unit) RunBenchmark(b *testing.B) {
 	if u.br {
 		panic("benchmark already ran")
