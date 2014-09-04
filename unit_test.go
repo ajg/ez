ecedfdb4816e06db3e471c81cad9de0ed3933336
Alvaro J. Genial <genial@alva.ro>
2014-09-03 20:11:42 -0400

    Add header

--- unit_test.go
+++ unit_test.go
@@ -1,3 +1,7 @@
+// Copyright 2014 Alvaro J. Genial. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package ez
 
 import (
@@ -8,7 +12,7 @@ import (
 
 func TestIn(t *testing.T) {
 	args := []interface{}{true, 42, "foo"}
-	cin := in{tuple{args}, "unit_test.go", 12}
+	cin := in{tuple{args}, "unit_test.go", 16}
 	if in := In(args...); !reflect.DeepEqual(cin, in) {
 		t.Errorf("In(%v)\nwant (%v)\nhave (%v)", args, cin, in)
 	}
@@ -16,7 +20,7 @@ func TestIn(t *testing.T) {
 
 func TestOut(t *testing.T) {
 	args := []interface{}{true, 42, "foo"}
-	cout := out{tuple{args}, "unit_test.go", 20, nil}
+	cout := out{tuple{args}, "unit_test.go", 24, nil}
 	if out := Out(args...); !reflect.DeepEqual(cout, out) {
 		t.Errorf("Out(%v)\nwant (%v)\nhave (%v)", args, cout, out)
 	}
@@ -24,7 +28,7 @@ func TestOut(t *testing.T) {
 
 func TestPanic(t *testing.T) {
 	p := errors.New("bar")
-	cout := out{tuple{}, "unit_test.go", 28, p}
+	cout := out{tuple{}, "unit_test.go", 32, p}
 	if out := Panic(p); !reflect.DeepEqual(cout, out) {
 		t.Errorf("Panic(%v)\nwant (%v)\nhave (%v)", p, cout, out)
 	}
