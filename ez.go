6c772f7f13ce943c6b9119bb16c7b13659db7ec1
Alvaro J. Genial <genial@alva.ro>
2019-07-02 04:21:58 -0400

    Add path style

--- ez.go
+++ ez.go
@@ -4,3 +4,29 @@
 
 // Package ez provides an easy, powerful way to define tests & benchmarks that are compatible with package `testing`.
 package ez
+
+var PathStyle = Absolute
+
+type Style int
+
+const (
+	Absolute Style = iota
+	Abstract
+	Relative
+	Truncate
+)
+
+func (s Style) String() string {
+	switch s {
+	case Absolute:
+		return "Absolute"
+	case Abstract:
+		return "Abstract"
+	case Relative:
+		return "Relative"
+	case Truncate:
+		return "Truncate"
+	default:
+		return ""
+	}
+}
